88d3f98f2aa84542c4176c25251815adb02b1c14
cognar <connor.mccloskey@gmail.com>
2022-09-17 18:44:49 -0700

    initializing project

---
+++ ChickenCoopFrontend/app/routes.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    states = {'InsideTemp': 70, 'InsideTempSP': 70, 'OutsideTemp': 60, 'InsideHum': 30, 'InsideHumSP': 30,
+              'OutsideHum': 20, 'HeatLampState': 1, 'DoorState': 1, 'FanState': 0}
+    chickens = "Shawna, Nat, Misty, Van, Tai, Lottie"
+    return render_template('index.html', chickens = chickens, states = states)
+
+@app.route('/helloesp')
+def helloHandler():
+    return 'Hello ESP!'
+
